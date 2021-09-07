91a7330dd3b0cfc34424c76c8c2071b22a60fd0b
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-09-07 21:58:28 +0300

    finish lab32

--- app/src/main/java/com/example/taskmaster/AddTaskActivity.java
+++ app/src/main/java/com/example/taskmaster/AddTaskActivity.java
@@ -5,13 +5,18 @@ import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Task;
+
 public class AddTaskActivity extends AppCompatActivity {
-    AppDatabase appDatabase;
+//    AppDatabase appDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +38,30 @@ public class AddTaskActivity extends AppCompatActivity {
                 String body = taskBody.getText().toString();
                 String state = taskState.getText().toString();
 
-                appDatabase = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "tasks").allowMainThreadQueries().fallbackToDestructiveMigration().build();
+//                appDatabase = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "tasks").allowMainThreadQueries().fallbackToDestructiveMigration().build();
+//                // save input fields into object
+//                Task task = new Task(title,body,state);
+//                // save to db
+//                appDatabase.taskDao().insertAll(task);
 
-                // save input fields into object
-                Task task = new Task(title,body,state);
-                // save to db
-                appDatabase.taskDao().insertAll(task);
+      //////////////////////////////////////////////// lab 32 //////////////////////////////////////////
+                // save data to data base
+                Task task = Task.builder()
+                        .title(title)
+                        .body(body)
+                        .status(state)
+                        .build();
+
+                Amplify.API.mutate(
+                        ModelMutation.create(task),
+                        response -> Log.i("MyAmplifyApp", "Added Todo with id: " + response.getData().getId()),
+                        error -> Log.e("MyAmplifyApp", "Create failed", error)
+                );
+
                 // redirect to menu page
                 Intent goToHomePage = new Intent(AddTaskActivity.this, MainActivity.class);
                 startActivity(goToHomePage);
             }
         });
-
-
     }
 }
